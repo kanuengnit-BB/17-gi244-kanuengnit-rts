21b46a679f0790f24d6448592e30505f06d44f3c
kanuengnit-BB <151009870+kanuengnit-BB@users.noreply.github.com>
2024-01-18 10:38:49 +0700

    add corner && zoom

--- Assets/Scripts/CamController.cs
+++ Assets/Scripts/CamController.cs
@@ -9,8 +9,17 @@ public class CamController : MonoBehaviour
     [SerializeField] private Camera cam;
 
     [Header("Move")] private float moveSpeed;
+    [SerializeField] private Transform corner1;
+    [SerializeField] private Transform corner2;
     [SerializeField] private float xInput;
     [SerializeField] private float zInput;
+    
+    [Header("Zoom")]
+    [SerializeField] private float zoomSpeed;
+    [SerializeField] private float zoomModifier;
+    [SerializeField] private float minZoomDist;
+    [SerializeField] private float maxZoomDist;
+    [SerializeField] private float dist;
 
     public static CamController instance;
 
@@ -24,12 +33,18 @@ public class CamController : MonoBehaviour
     void Start()
     {
         moveSpeed = 50;
+        //zoom setting
+        zoomSpeed = 25;
+        minZoomDist = 15;
+        maxZoomDist = 50;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         MoveByKB();
+        Zoom();
     }
 
     void MoveByKB()
@@ -39,5 +54,31 @@ public class CamController : MonoBehaviour
 
         Vector3 dir = (transform.forward * zInput) + (transform.right * xInput);
         transform.position += dir * moveSpeed*Time.deltaTime;
+        transform.position = Clamp(corner1.position, corner2.position);
+    }
+    private Vector3 Clamp(Vector3 lowerLeft, Vector3 topRight)
+    {
+        Vector3 pos = new Vector3(Mathf.Clamp(transform.position.x,
+                lowerLeft.x, topRight.x),
+            transform.position.y,
+            Mathf.Clamp(transform.position.z,
+                lowerLeft.z, topRight.z));
+        return pos;
+    }
+    
+    private void Zoom()
+    {
+        zoomModifier = Input.GetAxis("Mouse ScrollWheel");
+        if (Input.GetKey(KeyCode.Z))
+            zoomModifier = 0.01f;
+        if (Input.GetKey(KeyCode.X))
+            zoomModifier = -0.01f;
+        dist = Vector3.Distance(transform.position,cam.transform.position);
+        
+        if (dist < minZoomDist && zoomModifier > 0f)
+            return; //too close
+        else if (dist > maxZoomDist && zoomModifier < 0f)
+            return; //too far
+        cam.transform.position += cam.transform.forward * zoomModifier * zoomSpeed;
     }
 }
