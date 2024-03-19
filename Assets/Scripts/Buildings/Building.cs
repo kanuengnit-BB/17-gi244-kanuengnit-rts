35458bdfe5ed58d0a4409b16cc4f63d447fa6bf5
kanuengnit-BB <kanuengnit.mali@bumail.net>
2024-03-19 18:55:21 +0700

    builder success

--- Assets/Scripts/Buildings/Building.cs
+++ Assets/Scripts/Buildings/Building.cs
@@ -22,6 +22,11 @@ public class Building : Structure
 
     [SerializeField] private float curUnitWaitTime = 0f;
     [SerializeField] private bool isFunctional;
+    public bool IsFunctional
+    {
+        get { return isFunctional; }
+        set { isFunctional = value; }
+    }
 
     [SerializeField] private bool isHQ;
     public bool IsHq
@@ -34,12 +39,12 @@ public class Building : Structure
     {
         get { return intoTheGround; }
     }
-    public bool IsFunctional
-    {
-        get { return isFunctional; }
-        set { isFunctional = value; }
-    }
     
+    private float timer = 0f; //Constructing timer
+    public float Timer { get { return timer; } set { timer = value; } }
+    private float waitTime = 0.5f; //How fast it will be construct, higher is longer
+    public float WaitTime { get { return waitTime; } set { waitTime = value; } }
+
     // Start is called before the first frame update
     void Start()
     {
--- Assets/Scripts/Units_Script/Builder.cs
+++ Assets/Scripts/Units_Script/Builder.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class Builder : MonoBehaviour
 {
@@ -9,18 +10,37 @@ public class Builder : MonoBehaviour
     [SerializeField] private bool showGhost = false; //ghost building is showing
 
     [SerializeField] private GameObject[] buildingList; // Buildings that this unit can build
-    public GameObject[] BuildingList { get { return buildingList; } }
+
+    public GameObject[] BuildingList
+    {
+        get { return buildingList; }
+    }
+
     [SerializeField] private GameObject[] ghostBuildingList; // Transparent buildings according to building list
 
     [SerializeField] private GameObject newBuilding; // Current building to build
-    public GameObject NewBuilding { get { return newBuilding; } set { newBuilding = value; } }
+
+    public GameObject NewBuilding
+    {
+        get { return newBuilding; }
+        set { newBuilding = value; }
+    }
 
     [SerializeField] private GameObject ghostBuilding; // Tranparent building to check site to build
-    public GameObject GhostBuilding { get { return ghostBuilding; } set { ghostBuilding = value; } }
+
+    public GameObject GhostBuilding
+    {
+        get { return ghostBuilding; }
+        set { ghostBuilding = value; }
+    }
 
     [SerializeField] private GameObject inProgressBuilding; // The building a unit is currently building
 
-    public GameObject InProgressBuilding { get { return inProgressBuilding; } set { inProgressBuilding = value; } }
+    public GameObject InProgressBuilding
+    {
+        get { return inProgressBuilding; }
+        set { inProgressBuilding = value; }
+    }
 
     private Unit unit;
 
@@ -28,7 +48,7 @@ public class Builder : MonoBehaviour
     {
         unit = GetComponent<Unit>();
     }
-    
+
     void Update()
     {
         if (unit.State == UnitState.Die)
@@ -38,11 +58,33 @@ public class Builder : MonoBehaviour
         {
             GhostBuildingFollowsMouse();
 
-            if (Input.GetKeyDown(KeyCode.Escape) || Input.GetMouseButtonDown(1))
-                CancelToBuild();
+            if (Input.GetMouseButtonDown(0))
+            {
+                if (EventSystem.current.IsPointerOverGameObject())
+                {
+                    return;
+                }
+
+                CheckClickOnGround();
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape) || Input.GetMouseButtonDown(1))
+        {
+            CancelToBuild();
+        }
+
+        switch (unit.State)
+        {
+            case UnitState.MoveToBuild:
+                MoveToBuild(inProgressBuilding);
+                break;
+            case UnitState.BuildProgress:
+                BuildProgress();
+                break;
         }
     }
-    
+
     public void ToCreateNewBuilding(int i) //Start call from ActionManager UI Btns
     {
         if (buildingList[i] == null)
@@ -64,7 +106,7 @@ public class Builder : MonoBehaviour
             showGhost = true;
         }
     }
-    
+
     private void GhostBuildingFollowsMouse()
     {
         if (showGhost)
@@ -81,7 +123,7 @@ public class Builder : MonoBehaviour
             }
         }
     }
-    
+
     private void CancelToBuild()
     {
         toBuild = false;
@@ -95,7 +137,7 @@ public class Builder : MonoBehaviour
 
     public void BuilderStartFixBuilding(GameObject target)
     {
-        inProgressBuilding = target;        
+        inProgressBuilding = target;
         unit.SetState(UnitState.MoveToBuild);
     }
 
@@ -125,7 +167,7 @@ public class Builder : MonoBehaviour
         buildingObj.transform.position = new Vector3(buildingObj.transform.position.x,
             buildingObj.transform.position.y - building.IntoTheGround,
             buildingObj.transform.position.z);
-        
+
         //Set building's parent game object
         buildingObj.transform.parent = unit.Faction.BuildingsParent.transform;
 
@@ -150,6 +192,7 @@ public class Builder : MonoBehaviour
         //order builders to build together
         StartConstruction(inProgressBuilding);
     }
+
     private void CheckClickOnGround()
     {
         Ray ray = CamController.instance.Cam.ScreenPointToRay(Input.mousePosition);
@@ -166,7 +209,76 @@ public class Builder : MonoBehaviour
             }
         }
     }
-    
-    
 
+    private void MoveToBuild(GameObject b)
+    {
+        if (b == null)
+            return;
+
+        unit.NavAgent.SetDestination(b.transform.position);
+        unit.NavAgent.isStopped = false;
     }
+
+    private void BuildProgress()
+    {
+
+        if (inProgressBuilding == null)
+            return;
+
+        unit.LookAt(inProgressBuilding.transform.position);
+        Building b = inProgressBuilding.GetComponent<Building>();
+
+        //building is already finished
+        if ((b.CurHP >= b.MaxHP) && b.IsFunctional)
+        {
+            inProgressBuilding = null; //Clear this job off his mind
+            unit.SetState(UnitState.Idle);
+            return;
+        }
+
+        //constructing
+        b.Timer += Time.deltaTime;
+
+        if (b.Timer >= b.WaitTime)
+        {
+            b.Timer = 0;
+            b.CurHP++;
+
+            if (b.IsFunctional == false) //if this building is being built, not being fixed
+                //Raise up building from the ground
+                inProgressBuilding.transform.position += new Vector3(0f, b.IntoTheGround / (b.MaxHP - 1), 0f);
+
+            if (b.CurHP >= b.MaxHP) //finish
+            {
+                b.CurHP = b.MaxHP;
+                b.IsFunctional = true;
+
+                inProgressBuilding = null; //Clear this job off his mind
+                unit.SetState(UnitState.Idle);
+            }
+        }
+    }
+
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (unit.State == UnitState.Die)
+            return;
+
+        if (unit != null)
+        {
+            if (other.gameObject == inProgressBuilding)
+            {
+                unit.NavAgent.isStopped = true;
+                unit.SetState(UnitState.BuildProgress);
+            }
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if (ghostBuilding != null)
+            Destroy(ghostBuilding);
+    }
+}
+    
